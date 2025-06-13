fae3e4abc8895b40b605bc4888f7c594f40c908b
Nikhildilliwar007 <dilliwarnikhil@gmail.com>
2025-06-13 18:14:08 +0530

    2nd version

--- server.py
+++ server.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route('/', methods=["GET"])
 def root():
-  return "welcome to the Nikhil's app Version-V1"
+  return "welcome to the Nikhil's app Version-V22	"
 
 app.run(host="0.0.0.0", port=3000, debug=True)
